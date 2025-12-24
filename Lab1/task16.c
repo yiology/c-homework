db949bd11bb979dd3f8694a5019e440134dc4055
yiology <120478794+yiology@users.noreply.github.com>
2025-12-24 22:05:26 +0800

    Update task16.c

--- Lab1/task16.c
+++ Lab1/task16.c
@@ -2,9 +2,21 @@
 #include <stdio.h>
 
 int main() {
-    printf("   * * * *\n");
-    printf("    * * *\n");
-    printf("     * *\n");
-    printf("      *\n");
+    int num1, num2;
+
+    printf("请输入2个正整数 (用空格隔开): ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("和: %d\n", num1 + num2);
+    printf("差: %d\n", num1 - num2);
+    printf("积: %d\n", num1 * num2);
+    
+    // 整数除法会丢弃小数。如果除数为0会崩溃
+    if(num2 != 0) {
+        printf("商: %d\n", num1 / num2);
+    } else {
+        printf("商: 除数不能为0\n");
+    }
+
     return 0;
 }
