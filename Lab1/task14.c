320d2b40d7b09a91f66762452824d44fb8bdd56e
yiology <120478794+yiology@users.noreply.github.com>
2025-12-24 22:02:00 +0800

    Create task14.c

---
+++ Lab1/task14.c
@@ -0,0 +1,8 @@
+//输出带引号的句子
+#include <stdio.h>
+
+int main() {
+    // 使用反斜杠 \" 来表示双引号
+    printf("\"Hope is a good thing and maybe the best of things. And no good thing ever dies\".\n");
+    return 0;
+}
