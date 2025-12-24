4e05aa46588dd89e8c60bc8280651297500bacfe
yiology <120478794+yiology@users.noreply.github.com>
2025-12-24 22:00:27 +0800

    Update and rename task13.md to task13.c

---
+++ Lab1/task13.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // sizeof 返回的是字节数
+    printf("The size of int is %lu bytes.\n", sizeof(int));
+    printf("The size of char is %lu bytes.\n", sizeof(char));
+    printf("The size of float is %lu bytes.\n", sizeof(float));
+    printf("The size of double is %lu bytes.\n", sizeof(double));
+    return 0;
+}
+        
+
--- Lab1/task13.md
+++
@@ -1,3 +0,0 @@
-在你的机器上获取int, char, float, double 等基本数据类型所占的字节数，并以如下形式输出。
-                        The size of xx is xx bytes.
-
